a57c212b78854910ddcff52a4a66e5f646d5573a
Akinny <akinyemiowolabii@gmail.com>
2022-09-09 16:12:00 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,12 +17,12 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	} else if (n == 0)
+	}else if (n == 0)
         {
-		        printf("%d is zero\n", n);
-        } else	
+	       	printf("%d is zero\n", n);
+        }else	
 	{
-	     printf("%d is positive\n", n);
+		printf("%d  positive\n", n);
 	}		
 	return(0);
 }
