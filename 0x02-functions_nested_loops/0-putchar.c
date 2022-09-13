6c992f2b977f67de4ec9db96d9992b56a7123790
Akinny <akinyemiowolabii@gmail.com>
2022-09-13 08:41:43 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * This program prints the _putchar command
+ * main : Entry point of the program
+ *
+ * _putchar():This program prints every letter and symbol of the _putchar command
  *
  * It is also include the the main header file
  *
+ * Return 0
  */
 char _putchar();
 int main(void)
@@ -20,6 +23,3 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-
-
-	
