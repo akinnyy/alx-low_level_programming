a9d4a468db3f06c2ed94026a2b551e05bcc7800c
Akinny <akinyemiowolabii@gmail.com>
2022-09-13 08:26:36 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * This program prints the _putchar command
+ *
+ * It is also include the the main header file
+ *
+ */
+char _putchar();
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
+
+	
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Here,we learn about how to build and manipulate functions within C! So interesting!
