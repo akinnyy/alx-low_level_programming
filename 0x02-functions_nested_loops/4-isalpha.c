a96f81c0d7bc46617948681b9132ffbcbd89d9c7
Akinny <akinyemiowolabii@gmail.com>
2022-09-13 18:09:36 -0700

    Isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,12 @@
 /**
  * _isalpha-main function
  *
- * @c>character for comparison
+ * @c: character for comparison
  *
  * Return: 1 (success) , 0 (failure)
  *
  */
-int _isalpha (int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
