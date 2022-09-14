4744767394acd6721b1d5f0d6bee72c35f320ed9
Akinny <akinyemiowolabii@gmail.com>
2022-09-13 19:26:07 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include"6-abs.c"
+
+/**
+ * print_last_digit- The main function here
+ *
+ * @n: the integer whose last digit is requested 
+ *
+ * Return: The last digit of integer n
+ *
+ */
+int print_last_digit(int n)
+{
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
+}
