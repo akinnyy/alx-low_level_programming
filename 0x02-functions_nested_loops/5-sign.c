5b7653d4469b188b6080ce41c3bd4b684cd308f4
Akinny <akinyemiowolabii@gmail.com>
2022-09-13 18:29:12 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - main function
+ *
+ * @n: character for comparison
+ *
+ * Return: 1 for positives,0 if zero, -1 for negatives
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);\
+}	
