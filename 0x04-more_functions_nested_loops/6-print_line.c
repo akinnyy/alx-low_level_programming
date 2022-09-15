e60a44804b69b56f68eac7e9f18302c4b44ce068
Akinny <akinyemiowolabii@gmail.com>
2022-09-15 05:07:26 -0700

    Solved

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers
+ *
+ * @n:integerspecifying the number of times to print char
+ */
+void print_line(int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		_putchar(_);
+	}
+	_putchar('\n');
+}
