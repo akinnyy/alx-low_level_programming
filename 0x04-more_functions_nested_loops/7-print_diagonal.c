ca0b10ef25356b0626e6de5ed0c062f6c6d624f5
Akinny <akinyemiowolabii@gmail.com>
2022-09-15 05:30:26 -0700

    Solved

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints the numbers
+ *
+ * @n:integer specifying the number of times to print char
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for(j = 0; j < n; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(_);
+			_putchar('\n');
+		}
+	}
+}
