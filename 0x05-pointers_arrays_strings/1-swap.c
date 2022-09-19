1d59fb021e966fc71cfc9bb7ffb8bfdd364c7260
Akinny <akinyemiowolabii@gmail.com>
2022-09-19 04:49:31 -0700

    Solved

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int -  The main function that swaps
+ *
+ * @a:The integer whose value is to be swapped
+ *
+ * @b:Second integer to be swapped with the first
+ *
+ * Return:Nothing
+ */
+void swap_int(int *a , int *b)
+{
+	int x = *a;
+	int y = *b;
+	*a =  y;
+	*b =x;
+}
