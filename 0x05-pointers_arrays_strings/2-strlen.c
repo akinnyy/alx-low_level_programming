25fa8daa2f1aa577d7b2b5e5af3fa9d328c558ab
Akinny <akinyemiowolabii@gmail.com>
2022-09-19 05:08:48 -0700

    Solved

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of any string
+ *
+ * @s:The string whose length is required
+ *
+ * Return:0
+ */
+int _strlen(char *s)
+{
+	len = strlen(*s);
+	return (len)
+}
