deecf7402bdcc1a1f449abfbb9311ec3d6cfc0c9
Akinny <akinyemiowolabii@gmail.com>
2022-09-19 18:21:41 -0700

    Solved

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _strcpy- copies string
+ *
+ * @src:The input string
+ *
+ * @dest:The output string
+ * Return:dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int j;
+
+	for (j = 0; j <= _strlen(src); j++)
+	{
+		dest[j]) = src[j];
+	}
+	return (dest);
+}
