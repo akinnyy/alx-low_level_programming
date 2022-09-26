01df2b2756d5f4abbf5e58773f38049ded2bc110
Akinny <akinyemiowolabii@gmail.com>
2022-09-26 15:25:23 -0700

    Solved

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,28 @@
+/**
+ * *_memcpy - main function
+ *
+ *   * @dest: destination to copy to
+ *
+ *    * @src: source to copy from
+ *
+ *     * @n: n amount of times
+ *
+ *      * Return: return the string value
+ *
+ *       */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+		unsigned int i;
+
+
+
+			for (i = 0; i < n; i++)
+
+					{
+
+								dest[i] = src[i];
+
+									}
+
+				return (dest);
