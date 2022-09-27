04b3465178c408a170cc6b54aff7f2cdb028b7b8
Akinny <akinyemiowolabii@gmail.com>
2022-09-26 18:48:36 -0700

    solved

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,35 +3,22 @@
 #include <stdio.h>
 
 /**
+ * print_diagsums - prints diagonal summaries
  *
- *  * print_diagsums - prints diagonal summaries
+ * @a: array to use
  *
- *   * @a: array to use
- *
- *    * @size: size of diagonal
- *
- *     */
+ * @size: size of diagonal
+ */
 void print_diagsums(int *a, int size)
 {
+	int i;
+	int diagO = 0;
+	int diagT = 0;
 
-		int i;
-
-			int diagO = 0;
-
-				int diagT = 0;
-
-
-
-					for (i = 0; i < size; i++)
-
-							{
-
-										diagO += a[(i * size) + i];
-
-												diagT += a[(size - 1) + ((size - 1) * i)];
-
-													}
-
-						printf("%d, %d\n", diagO, diagT);
-
+	for (i = 0; i < size; i++)
+	{
+		diagO += a[(i * size) + i];
+		diagT += a[(size - 1) + ((size - 1) * i)];
+	}
+	printf("%d, %d\n", diagO, diagT);
 }
