7b113a4d86b0e4e8ce10b998a0d931b3f351c575
Akinny <akinyemiowolabii@gmail.com>
2022-09-26 18:00:00 -0700

    solved

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,33 +1,23 @@
 #include "main.h"
 
 /**
+ * _memset - sets memory to array
  *
- *  * _memset - sets memory to array
+ * @s: array to set
  *
- *   * @s: array to set
+ * @b: value to set it as
  *
- *    * @b: value to set it as
+ * @n: n amount of times
  *
- *     * @n: n amount of times
- *
- *      * Return: char value of s
- *
- *       */
+ * Return: char value of s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
-		unsigned int i;
-
-
-
-			for (i = 0; i < n; i++)
-
-					{
-
-								s[i] = b;
-
-									}
-
-				return (s);
-
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
