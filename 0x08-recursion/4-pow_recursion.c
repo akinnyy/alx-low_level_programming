8aa57a217aca5b8d42a1d388927261f0ca77e678
Akinny <akinyemiowolabii@gmail.com>
2022-09-28 15:27:38 -0700

    Solved

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - main function that returns int x raised to int y
+ *
+ * @x: The base number
+ * @y: The power number
+ *
+ * Return: the value of x raised to y
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, (y - 1)));
+}
