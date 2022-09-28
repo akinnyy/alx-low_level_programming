b9a7071a785b784995347ed4b5f028e1e61f8847
Akinny <akinyemiowolabii@gmail.com>
2022-09-28 14:22:06 -0700

    Solved

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - main function that recursively prints strings
+ *
+ * @s: The string to be printed
+ *
+ * Return : Nothing
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
