608aa97e483e81805cac3ab4b6877edb9dbb8b2b
Akinny <akinyemiowolabii@gmail.com>
2022-09-30 15:41:49 -0700

    Solved

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _atoi - converts string to integer
+ *
+ * @s: string to convert
+ *
+ * Return: returns integer value
+ *
+ */
+int _atoi(char *s)
+{
+	int k = 0;
+	int np = 0;
+	int len = 0;
+	int d = 0;
+	int num = 0;
+	int flo = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (k < len && flo == 0)
+	{
+		if (s[k] == '-')
+			++d;
+		if (s[k] >= '0' && s[k] <= '9')
+		{
+			num = s[k] - '0';
+			if (d % 2)
+				num = -num;
+			np = np * 10 + num;
+			flo = 1;
+			if (s[k + 1] < '0' || s[k + 1] > '9')
+				break;
+			flo = 0;
+		}
+		k++;
+
+
+	}
+	if (flo == 0)
+		return (0);
+	return (np);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of any string
+ *
+ * @s:The string whose length is required
+ *
+ * Return:length o the string
+ */
+int _strlen(char *s)
+{
+	int j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+	}
+	return (j);
+}
