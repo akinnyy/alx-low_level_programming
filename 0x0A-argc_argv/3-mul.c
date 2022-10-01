bef77143908c2ea83c1aa98488fd9f5cfe2a062e
Akinny <akinyemiowolabii@gmail.com>
2022-09-30 18:14:45 -0700

    Solved

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ *
+ * @argc: argument c
+ *
+ * @argv: vector of arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc != 3)
+		printf("Error\n");
+	return (1);
+	else
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	return (0);
+}
