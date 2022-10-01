99b4ec6e5cdde7a748f31b499b5e71ef8aa2a0fe
Akinny <akinyemiowolabii@gmail.com>
2022-09-30 17:25:44 -0700

    Solved

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ *
+ * @argc: argument c
+ *
+ * @argv: vector of arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	void(*argv[]);
+	printf("%s\n", argc);
+	return (0);
+}
