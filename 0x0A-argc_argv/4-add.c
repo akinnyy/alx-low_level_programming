3035192da6c01e1b454a2d07643e2fbea9a4accd
Akinny <akinyemiowolabii@gmail.com>
2022-09-30 19:48:01 -0700

    Solved

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,4 @@
-nclude "holberton.h"
+#include "main.h"
 
 #include <stdio.h>
 
@@ -14,95 +14,55 @@ nclude "holberton.h"
  * @s: string to check
  *
  * Return: boolean
+ *
  */
 int StringCheck(char *s)
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			for (; s[i] != '\0'; i++)
-
-					{
-
-								if (!isdigit(s[i]))
-
-											{
-
-															return (0);
-
-																	}
-
-									}
-
-				return (1);
-
+	for (; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
-
 /**
  * main - main function
  *
- * @argc: argumentc
+ * @argc: argument c
  *
- * @argv: vector of argument
+ * @argv: vector of arguments
  *
  * Return: always 0
- *
  */
 int main(int argc, char  *argv[])
 {
+	int i;
+	int result = 0;
 
-		int i;
-
-			int result = 0;
-
-
-
-				if (argc > 1)
-
-						{
-
-									for (i = 1; i < argc; i++)
-
-												{
-
-																if (StringCheck(argv[i]))
-
-																				{
-
-																									result += atoi(argv[i]);
-
-																												}
-
-																			else
-
-																							{
-
-																												printf("Error\n");
-
-																																return (1);
-
-																																			}
-
-																					}
-
-											printf("%d\n", result);
-
-													return (0);
-
-														}
-
-					else
-
-							{
-
-										printf("%d\n", 0);
-
-												return (1);
-
-													}
-
-
-
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (StringCheck(argv[i]))
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
 }
