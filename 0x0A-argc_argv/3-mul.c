4f2381d9773f8f2dc67dc5d92195c93619d9512f
Akinny <akinyemiowolabii@gmail.com>
2022-09-30 18:25:53 -0700

    Solved

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,8 +18,7 @@ int main(int argc, char *argv[])
 	return (1);
 	else
 	{
-		sum = (atoi(argv[1]) * atoi(argv[2]));
-		printf("%d\n", sum);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	return (0);
 }
