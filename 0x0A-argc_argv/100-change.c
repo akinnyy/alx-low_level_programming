dca78eddcd258861e4050f8c8206bc26966a3186
Akinny <akinyemiowolabii@gmail.com>
2022-09-30 20:20:57 -0700

    Solved

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,99 +1,55 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
 
 /**
+ * main - main function
  *
- *  * main - main function
+ * @argc: argument c 
  *
- *   * @argc: argumentc
+ * @argv: vector of arguments
  *
- *    * @argv: vector of arguments
+ * Return: always 0
  *
- *     *Return: always 0
- *
- *      */
-
+ */
 int main(int argc, char  *argv[])
-
 {
+	int coins = 0;
 
-		int coins = 0;
+	if (argc == 2)
+	{
+		if (strchr(argv[argc - 1], '-'))
+		{
+			printf("0\n");
+			return (1);
+		}
+		int money;
 
-
-
-			if (argc == 2)
-
-					{
-
-								if (strchr(argv[argc - 1], '-'))
-
-											{
-
-															printf("0\n");
-
-																		return (1);
-
-																				}
-
-										int money;
-
-
-
-												money = atoi(argv[argc - 1]);
-
-
-
-														while (money > 0)
-
-																	{
-
-																					if (money % 25 == 0)
-
-																									{
-
-																														money -= 25;
-
-																																	} else if (money % 10 == 0)
-
-																																					{
-
-																																										money -= 10;
-
-																																													} else if (money % 5 == 0)
-
-																																																	{
-
-																																																						money -= 5;
-
-																																																									} else if (money % 2 == 0)
-
-																																																													{
-
-																																																																		money -= 2;
-
-																																																																					} else
-
-																																																																									{
-
-																																																																														money--;
-
-																																																																																	}
-
-																																																																								coins++;
-
-																																																																										}
-
-																printf("%d\n", coins);
-
-																		return (0);
-
-																			}
-
-				printf("Error\n");
-
-					return (1);
-
+		money = atoi(argv[argc - 1]);
+		while (money > 0)
+		{
+			if (money % 25 == 0)
+			{
+				money -= 25;
+			}
+			else if (money % 10 == 0)
+			{
+				money -= 10;
+			} else if (money % 5 == 0)
+			{
+				money -= 5;
+			} else if (money % 2 == 0)
+			{
+				money -= 2;
+			} else
+			{
+				money--;
+			}
+			coins++;
+		}
+		printf("%d\n", coins);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
 }
