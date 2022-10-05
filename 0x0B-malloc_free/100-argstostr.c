cdeb31ce8c0c20f5848a68d2e6790c325d5354da
Akinny <akinyemiowolabii@gmail.com>
2022-10-04 17:51:21 -0700

    Solved

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,80 +11,76 @@ i#include "main.h"
  *
  * Return: value
  */
-char **helper(int word, int len, char *str, char **s)
+char *argstostr(int ac, char **av)
+
 {
-	int i, k, j;
 
-	j = 0;
-	for (i = 0; i < word; i++)
-	{
-		k = 0;
-		for (; j < len; j++)
-		{
-			if (str[0] != ' ' || str[j] != ' ')
-			{
-				s[i][k] = str[j];
-				k++;
-			}
-			if (j != 0 && str[j] == ' ' && str[j - 1] != ' ')
-			{
-				j++;
-				break;
-			}
-		}
-		s[i][k + 1] = '\0';
-	}
-	s[word + 1] = NULL;
-	return (s);
-}
-/**
- * strtow - string to words
- * @str: string to check
- * Return: return char value
- */
-char **strtow(char *str)
-{
-	int len, i, j, size, k, word;
-	char **s;
+		int size;
 
-	if (str == NULL)
-		return (NULL);
-	len = 0;
-	word = 0;
+			char *s;
 
-	while (str[len] != '\0')
-	{
-		if (str[0] != ' ')
-			word++;
-		if (str[len] != ' ' && str[len - 1] == ' ' && len != 0)
-			word++;
-		len++;
-	}
-	s = (char **)malloc(sizeof(char *) * word + 1);
-	if (s == NULL)
-		return (NULL);
-	j = 0;
+				int i;
 
-	for (i = 0; i < word; i++)
-	{
-		size = 0;
+					int j;
 
-		for (; j < len; j++)
-		{
-			if (str[0] != ' ' || str[j] != ' ')
-				size++;
-			if (str[j] == ' ' && size > 0)
-				break;
-		}
-		printf("%d\n", size);
-		s[i] = (char *)malloc(sizeof(char) * size + 1);
-		if (s[i] == NULL)
-		{
-			for (k = i - 1; k >= 0; k--)
-				free(s[k]);
-			free(s);
-		}
-	}
-	s = helper(word, len, str, s);
-	return (s);
+						int k;
+
+
+
+							if (ac == 0 || av == NULL)
+
+										return (NULL);
+
+
+
+								for (i = 0; i < ac; i++)
+
+										{
+
+													for (j = 0; av[i][j] != '\0'; j++)
+
+																	size++;
+
+														}
+
+
+
+									size += (ac + 1);
+
+										s = malloc(sizeof(char) * size);
+
+											if (s == NULL)
+
+														return (NULL);
+
+												k = 0;
+
+													for (i = 0; i < ac; i++)
+
+															{
+
+																		for (j = 0; av[i][j] != '\0'; j++)
+
+																					{
+
+																									s[k] = av[i][j];
+
+																												k++;
+
+																														}
+
+																				s[k] = '\n';
+
+																						k++;
+
+																							}
+
+
+
+														s[k] = '\0';
+
+
+
+															return (s);
+
 }
