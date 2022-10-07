07116a05b22681b2c1fe009440ae231c059d0d9a
Akinny <akinyemiowolabii@gmail.com>
2022-10-07 14:28:16 -0700

    Solved

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - main function
+ *
+ * @b:value to be allocated memory for
+ *
+ * Return: Pointer  (success) , 98 (failure)
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *t;
+
+	t = malloc(b);
+	if (t == NULL)
+	       return (98);
+	return (t);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+This project exploremore learning about memory allocation using malloc().
+Different manipulations of memory allocation are exemplified and exercised.
