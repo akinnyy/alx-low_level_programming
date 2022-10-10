a3768fec0aafff2658946782a760221c7075b067
Akinny <akinyemiowolabii@gmail.com>
2022-10-10 16:45:40 -0700

    Solved

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-
 #include "dog.h"
 
 /**
+ * init_dog - initializes a dog
  *
- *  * init_dog - initializes a dog
- *
- *   * @d: address of dog
- *
- *    * @name: name of dog
- *
- *     * @age: age of dog
- *
- *      * @owner: owner of dog
- *
- *       */
-
+ * @d: address of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
