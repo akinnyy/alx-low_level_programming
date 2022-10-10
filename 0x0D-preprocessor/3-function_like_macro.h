77909a699e06cf6a6bfdb48b56cdeb7866feda09
Akinny <akinyemiowolabii@gmail.com>
2022-10-10 13:20:41 -0700

    Solved

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef 3_FUNCTION_LIKE_MACRO_H
-#define 3_FUNCTION_LIKE_MACRO_H
+#ifndef _3_FUNCTION_LIKE_MACRO_H
+#define _3_FUNCTION_LIKE_MACRO_H
 #define ABS(x) ((x) < 0 ? -1 * (x) : (x))
-#endif /*3_FUNCTION_LIKE_MACRO_H*/
+#endif /*_3_FUNCTION_LIKE_MACRO_H*/
