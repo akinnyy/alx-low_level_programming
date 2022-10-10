4da5f27f625ec79805ab2248700627861224cb1a
Akinny <akinyemiowolabii@gmail.com>
2022-10-10 16:34:57 -0700

    Solved

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,7 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 typedef struct dog dog_t;
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
