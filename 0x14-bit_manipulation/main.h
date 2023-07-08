7ac1033e4516f702ebade9b50ef564b6a36eab62
Akinyemi Owolabi <friday4real567@yahoo.com>
2023-07-07 20:08:05 -0700

    Bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,8 +7,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 int _putchar(char c);
-unsigned int binary_to_uint(const char *b);
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
+unsigned int binary_to_uint(const char *b)i;
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+int get_endianness(void);
 #endif
